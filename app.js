bd6ad17ee829492068c6b6c032bdb87042e8f404
Sophia Pozzi <sopozzi@gmail.com>
2016-09-28 18:26:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+$(function(){
+	$('#search').submit(function(event){
+		event.preventDefault();
+		var searchTerm = $('#query').val();
+		getRequest(searchTerm);
+	});
+});
+function getRequest(searchTerm, pageToken){
+	var params = {
+		part: 'snippet',
+		r: 'json',
+		key: 'AIzaSyBmW7ZkBI0B1urmprC4ZGXZ4sfoDEdmlP8',
+		q: searchTerm,
+		maxResults: 10,
+	};
+	if (pageToken){
+		params.pageToken = pageToken;
+	}
+	url = 'https://www.googleapis.com/youtube/v3/search'
+	$.getJSON(url, params, function(data){
+		showResults(data);
+	});
+}
+function showResults(results) {
+	var html = "";
+	console.log(results);
+	$.each(results.items, function(index,value){
+		//console.log(value);
+		html += '<p><a href="https://www.youtube.com/watch?v='+ value.id.videoId +'">';
+		html += '<img src=" '+ value.snippet.thumbnails.medium.url + '"/></a></p>';
+	});
+	$('#search-results').html(html);
+}
